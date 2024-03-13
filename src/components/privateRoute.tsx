493518ca31665c7099154251dbfbb2bddeecbd23
Iurii Venger <macbook@air>
2024-03-13 10:37:33 +0100

    update privateRoute mechanic

--- src/components/privateRoute.tsx
+++ src/components/privateRoute.tsx
@@ -4,19 +4,22 @@ import { redirect } from 'next/navigation';
 import { FC, useEffect } from 'react';
 
 import Loader from '@/components/Loader';
+import { RequestStatus } from '@/constants';
 import { useAppSelector } from '@/store';
-import { selectFinanceData, selectIsUserLoggedIn } from '@/store/selectors';
+import { selectFinanceData, selectIsUserLoggedIn, selectUser } from '@/store/selectors';
 
 const privateRoute = (Component: FC) => {
   const IsAuth: FC = (props) => {
     const isUserLoggedIn = useAppSelector(selectIsUserLoggedIn);
+    const { userLoadingStatus } = useAppSelector(selectUser);
+    const isUserLoading = userLoadingStatus === RequestStatus.PENDING;
     const { isAppInitialized } = useAppSelector(selectFinanceData);
 
     useEffect(() => {
-      if (!isUserLoggedIn && isAppInitialized) {
+      if (!isUserLoggedIn && isAppInitialized && !isUserLoading) {
         return redirect('/auth/login');
       }
-    }, [isAppInitialized]);
+    }, [isAppInitialized, isUserLoggedIn]);
 
     if (!isUserLoggedIn) {
       return <Loader />;
