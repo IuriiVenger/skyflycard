fdc8ef6d6b4cf1f2665286a201d40c4bd9fbc988
Iurii Venger <macbook@air>
2024-03-13 23:46:55 +0100

    add link to authLayout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,7 @@
 import 'react-toastify/dist/ReactToastify.css';
 import Image from 'next/image';
 
+import Link from 'next/link';
 import { FC } from 'react';
 
 import logo from '@/assets/svg/header_logo.svg';
@@ -10,9 +11,9 @@ type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 const RootLayout: FC<RootLayoutProps> = ({ children }) => (
   <main className="flex w-full flex-col items-center p-6 pt-20">
     <h1 className="mb-4 text-3xl font-semibold text-neutral-400">Welcome to</h1>
-
-    <Image src={logo} width={200} height={45} alt="logo" className="mb-20" />
-
+    <Link href="/">
+      <Image src={logo} width={200} height={45} alt="logo" className="mb-20" />
+    </Link>
     {children}
   </main>
 );
