0bbc94779499baa9b5031ff0a060ae3bc3fd583a
Iurii Venger <macbook@air>
2024-07-10 17:02:38 +0200

    add initData

--- src/components/Mini-App/index.tsx
+++ src/components/Mini-App/index.tsx
@@ -1,21 +1,25 @@
-import { initMiniApp } from '@telegram-apps/sdk';
+import { retrieveLaunchParams } from '@telegram-apps/sdk';
 import { useEffect, useState } from 'react';
 
 const MiniApp = () => {
   const [contacts, setContacts] = useState<any>();
+  const [initData, setInitData] = useState<any>();
 
   useEffect(() => {
-    const [miniApp] = initMiniApp();
-    miniApp.requestContact().then((contact) => {
-      console.log(contact);
-      setContacts(contact);
-    });
+    // const [miniApp] = InitData();
+    // miniApp.requestContact().then((contact) => {
+    //   console.log(contact);
+    //   setContacts(contact);
+    // });
+    const launchParams = retrieveLaunchParams();
+    setInitData(launchParams.initData);
   }, []);
 
   return (
     <div>
       <h1>Mini App</h1>
       {JSON.stringify(contacts)}
+      {JSON.stringify(initData)}
     </div>
   );
 };
