57aa10e4de93019210279576cd929c19bf0b389e
Iurii Venger <macbook@air>
2024-07-11 23:22:07 +0200

    disable responce interceptors

--- src/api/index.ts
+++ src/api/index.ts
@@ -31,55 +31,55 @@ instance.interceptors.request.use((config) => {
   return config;
 });
 
-const defaultErrorMessageForUnauthorized = 'You are not authorized. Please log in.';
+// const defaultErrorMessageForUnauthorized = 'You are not authorized. Please log in.';
 
-let isTokenRefreshing = false;
-let requestQueue: (() => void)[] = [];
+// let isTokenRefreshing = false;
+// let requestQueue: (() => void)[] = [];
 
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error?.response?.status === ResponseStatus.UNAUTHORIZED) {
-      const { response, config: failedRequest } = error;
-      const refreshToken = getCookie('refresh_token');
+// instance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error?.response?.status === ResponseStatus.UNAUTHORIZED) {
+//       const { response, config: failedRequest } = error;
+//       const refreshToken = getCookie('refresh_token');
 
-      if (response.config?.url.includes('/auth/refresh/refresh_token') || !refreshToken) {
-        if (typeof window !== 'undefined') {
-          toast.error(error?.response?.data?.message || defaultErrorMessageForUnauthorized);
+//       if (response.config?.url.includes('/auth/refresh/refresh_token') || !refreshToken) {
+//         if (typeof window !== 'undefined') {
+//           toast.error(error?.response?.data?.message || defaultErrorMessageForUnauthorized);
 
-          navigate('/auth/login');
-        }
-        deleteTokens();
-        requestQueue = [];
-        return Promise.reject(response);
-      }
-      if (!isTokenRefreshing && typeof refreshToken === 'string') {
-        isTokenRefreshing = true;
-        refreshTokens(refreshToken)
-          .then((newTokens) => {
-            if (newTokens?.access_token) {
-              setTokens(newTokens);
-              requestQueue.forEach((request) => request());
-              requestQueue = [];
-            }
-          })
-          .finally(() => {
-            isTokenRefreshing = false;
-          });
-      }
-      return new Promise((resolve) => {
-        requestQueue.push(() => {
-          resolve(instance(failedRequest));
-        });
-      });
-    }
+//           navigate('/auth/login');
+//         }
+//         deleteTokens();
+//         requestQueue = [];
+//         return Promise.reject(response);
+//       }
+//       if (!isTokenRefreshing && typeof refreshToken === 'string') {
+//         isTokenRefreshing = true;
+//         refreshTokens(refreshToken)
+//           .then((newTokens) => {
+//             if (newTokens?.access_token) {
+//               setTokens(newTokens);
+//               requestQueue.forEach((request) => request());
+//               requestQueue = [];
+//             }
+//           })
+//           .finally(() => {
+//             isTokenRefreshing = false;
+//           });
+//       }
+//       return new Promise((resolve) => {
+//         requestQueue.push(() => {
+//           resolve(instance(failedRequest));
+//         });
+//       });
+//     }
 
-    toast.error(
-      error?.response?.data?.message || error?.response?.data?.error || error?.message || 'Something went wrong',
-    );
-    return Promise.reject(error);
-  },
-);
+//     toast.error(
+//       error?.response?.data?.message || error?.response?.data?.error || error?.message || 'Something went wrong',
+//     );
+//     return Promise.reject(error);
+//   },
+// );
 
 interface RequestParams {
   params?: object;
