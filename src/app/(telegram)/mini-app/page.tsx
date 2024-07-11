0b1e736f54ee7d43d736de679a3228835ca7f449
Iurii Venger <macbook@air>
2024-07-11 10:51:15 +0200

    add miniAppRequestContact

--- src/app/(telegram)/mini-app/page.tsx
+++ src/app/(telegram)/mini-app/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { SDKProvider } from '@telegram-apps/sdk-react';
+
 import MiniApp from '@/components/Mini-App';
 
 // import { initMiniApp } from '@telegram-apps/sdk';
 // import { useState } from 'react';
 
-const MiniAppPage = () => <MiniApp />;
+const MiniAppPage = () => (
+  <SDKProvider>
+    <MiniApp />
+  </SDKProvider>
+);
 
 export default MiniAppPage;
