98f529901a230a5726dc940554bdccd7edac31b9
Iurii Venger <macbook@air>
2024-07-11 10:09:01 +0200

    add consoleLogInitData

--- src/components/Mini-App/index.tsx
+++ src/components/Mini-App/index.tsx
@@ -3,28 +3,27 @@ import { useEffect, useState } from 'react';
 
 const MiniApp = () => {
   const [contacts, setContacts] = useState<any>();
-  const [initData, setInitData] = useState<any>();
-
+  const [initialData, setInitialData] = useState<Record<string, string>>();
   useEffect(() => {
     // const [miniApp] = InitData();
     // miniApp.requestContact().then((contact) => {
     //   console.log(contact);
     //   setContacts(contact);
     // });
-    const launchParams = retrieveLaunchParams();
-    setInitData(Object.entries(launchParams.initData as any));
+    const { initData } = retrieveLaunchParams();
+    console.log(initData);
   }, []);
 
   return (
     <div>
       <h1>Mini App</h1>
 
-      {initData &&
+      {/* {initData &&
         (initData as Array<[string, string]>).map(([key, value]) => (
           <div key={key}>
             <strong>{key}</strong>: {value}
           </div>
-        ))}
+        ))} */}
     </div>
   );
 };
