7654f80c873ba4234ee6ec6a31e1badfd8b94fbf
Iurii Venger <macbook@air>
2024-07-12 09:56:08 +0200

    udpate initApp

--- src/store/components/StoreProvider.tsx
+++ src/store/components/StoreProvider.tsx
@@ -11,10 +11,17 @@ const StoreProvider: FC<PropsWithChildren> = ({ children }) => {
   const { dispatch } = store;
   const { initApp } = useInitApp(dispatch);
   const launchParams = useLaunchParams(true);
+
+  const currentSeconds = Math.floor(Date.now() / 1000);
+
   useEffect(() => {
     initApp();
-    console.log(launchParams?.initDataRaw, 'launchParams?.initDataRaw');
+    console.log(launchParams?.initDataRaw, currentSeconds, 'launchParams?.initDataRaw');
   }, []);
+
+  useEffect(() => {
+    console.log(launchParams?.initDataRaw, currentSeconds, 'launchParams?.initDataRaw');
+  }, [launchParams]);
 
   return <Provider store={store}>{children}</Provider>;
 };
