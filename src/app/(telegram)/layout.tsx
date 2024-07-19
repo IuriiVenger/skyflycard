897e3f38936405a11d8bcf2cdcc8faf50fc09c7d
Iurii Venger <macbook@air>
2024-07-19 18:06:16 +0200

    change place of TelegramEnv set

--- src/app/(telegram)/layout.tsx
+++ src/app/(telegram)/layout.tsx
@@ -7,26 +7,16 @@ import { FC, Suspense } from 'react';
 import TelegramAuth from './_components/TelegramAuth';
 
 import { BrandLoader } from '@/components/ui/Loader';
-import { AppEnviroment } from '@/constants';
-import { useAppDispatch } from '@/store';
-import { setAppEnviroment } from '@/store/slices/config';
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
-const MainLayout: FC<RootLayoutProps> = ({ children }) => {
-  const dispatch = useAppDispatch();
-  dispatch(setAppEnviroment(AppEnviroment.TELEGRAM));
-
-  localStorage.setItem('app_enviroment', AppEnviroment.TELEGRAM);
-
-  return (
-    <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
-      <SDKProvider>
-        <TelegramAuth />
-        <Suspense fallback={<BrandLoader />}>{children}</Suspense>
-      </SDKProvider>
-    </main>
-  );
-};
+const MainLayout: FC<RootLayoutProps> = ({ children }) => (
+  <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
+    <SDKProvider>
+      <TelegramAuth />
+      <Suspense fallback={<BrandLoader />}>{children}</Suspense>
+    </SDKProvider>
+  </main>
+);
 
 export default MainLayout;
